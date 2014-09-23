26a997eec0f728a3ea860ee930a5285c17f36ca4
Matthew Jones <m.t.h.jones@gmail.com>
2014-09-22 20:06:55 -0600

    Clarify main function input handling

--- dp181e/src/dp181e/Main.scala
+++ dp181e/src/dp181e/Main.scala
@@ -5,17 +5,14 @@ import dp181e.Equations.Equation
 object Main {
   def main(args: Array[String]) {
     println("Equation solver! Enter equations on separate lines. Enter q to quit.")
-    var prevEq: Option[Equation] = None
     for (line <- io.Source.stdin.getLines().takeWhile(_ != "q")) {
       Equation.parse(line) match {
-        case Some(e) => prevEq match {
-          case Some(e2) =>
-            e.intersect(e2) match {
-              case Some(i) => println(i)
-              case None => println("Parallel lines, no intersection")
-            }
-            prevEq = None
-          case None => prevEq = Some(e)
+        case Some(e1) => io.Source.stdin.getLines().map(Equation.parse).next() match {
+          case Some(e2) => e1.intersect(e2) match {
+            case Some(i) => println(i)
+            case None => println("Parallel lines, no intersection.")
+          }
+          case None => println("Invalid equation.")
         }
         case None => println("Invalid equation.")
       }
